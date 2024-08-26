966b34667b98adf36038af6d1ef33ab4e9fbcfc3
Paweł Leśnik <pawel.lesnik@tdy.pl>
2024-08-26 19:48:45 +0200

    change

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -9,13 +9,13 @@ type ProductListItemProos = {
 
 export const ProductListItem = ({ product }: ProductListItemProos) => {
 	return (
-		<div className="mb-4">
+		<li className="mb-4">
 			<Link href={`/products/${product.id}`}>
 				<article className="rounded-lg bg-white p-4 shadow-md">
 					<ProductCoverImage {...product.coverImage} />
 					<ProductListItemDescription product={product} />
 				</article>
 			</Link>
-		</div>
+		</li>
 	);
 };
--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -5,9 +5,9 @@ export const ProductList = ({ products }: { products: ProductItemType[] }) => {
 	return (
 		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{products.map((product) => (
-				<span key={product.id}>
+				<ul key={product.id}>
 					<ProductListItem data-testid="products-list" product={product} />
-				</span>
+				</ul>
 			))}
 		</div>
 	);
