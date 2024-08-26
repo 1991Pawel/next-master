9eb3713c010c44e6fbc1422c55848f1c94d155a1
Paweł Leśnik <pawel.lesnik@tdy.pl>
2024-08-26 19:42:53 +0200

    change data id

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,13 +3,10 @@ import { type ProductItemType } from "../types";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => {
 	return (
-		<div
-			data-testid="products-list"
-			className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-		>
+		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{products.map((product) => (
 				<span key={product.id}>
-					<ProductListItem product={product} />
+					<ProductListItem data-testid="products-list" product={product} />
 				</span>
 			))}
 		</div>
